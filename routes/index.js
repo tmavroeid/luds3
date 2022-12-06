0962e9a6f6831118a74f024ce38d128aff4a63b4
teo <tmavroeidakos@gmail.com>
2022-12-06 23:45:23 +0200

    feat: add routes under one file.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
-// Require controller modules.
-var s3_controller = require('../controllers/s3_controller');
+const express = require('express')
+const router = express.Router()
+const { s3listing, s3download, apiS3listing, apiS3download } = require('../controllers/index')
 // The middleware functions that will handle the request about listing the S3 object is handled by the s3_controller.js
 // Here the request about listing is routed to the s3_controller and its function named s3_directory_listing_get
- router.get('/', s3_controller.s3_directory_listing_get);
+router.get('/list', s3listing)
+router.get('/download/:key(*)', s3download)
 
+router.get('/api/list', apiS3listing)
+router.get('/api/download/:key(*)', apiS3download)
 
-
-module.exports = router;
+module.exports = router
