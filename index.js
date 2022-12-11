8e1a8b09c16be931b40682437d64084a53141877
teo <tmavroeidakos@gmail.com>
2022-12-11 18:04:48 +0200

    feat

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
 const { program } = require('commander')
-const { setupCredentials, getCredentials, listing, download } = require(__dirname + '/beskad.js')
+const path = require('path')
+const logger = require('./common/logger')
+const { setupCredentials, getCredentials, listing, download, deploy } = require(path.join(__dirname, '/beskad.js'))
 
 program
   .command('set-credentials')
-  .requiredOption('-access-key, --access-key <value>', 'The access key for IAM user.')
-  .requiredOption('-secret-access-key, --secret-access-key <value>', 'The secret access key for IAM user.')
+  .requiredOption('-accesskey, --accesskey <value>', 'The access key for IAM user.')
+  .requiredOption('-secretkey, --secretkey <value>', 'The secret access key for IAM user.')
   .option('-region, --region <value>', 'The region where S3 buckets reside.')
   .description('Setup aws IAM user credentials to access S3 resources.')
   .action(setupCredentials)
@@ -14,6 +16,12 @@ program
   .command('get-credentials')
   .description('Get user credentials for IAM user.')
   .action(getCredentials)
+
+program
+  .command('run-api')
+  .requiredOption('-bucket, --bucket <value>', 'The bucket to list all items.')
+  .description('Deploys the api to list and download items from the bucket param.')
+  .action(deploy)
 
 program
   .command('list')
