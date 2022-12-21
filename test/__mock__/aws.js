c46a4f96c11b46e97d2764a04ba4bc0e327c7ae4
teo <tmavroeidakos@gmail.com>
2022-12-22 00:13:23 +0200

    feat: update tests.

--- test/__mock__/aws.js
+++ test/__mock__/aws.js
@@ -1,32 +1,28 @@
 /* eslint-env jest */
-const AWS = {
-  config: function () {
-    return {
-      update: function (options, allowUnknownKeys) {
-        allowUnknownKeys = allowUnknownKeys || false
-        options = this.extractCredentials(options)
-        return Promise.resolve(true)
-      }
+
+module.exports = {
+  config: {
+    update () {
+      return true
     }
   },
   S3: function () {
     return {
-      getObject: jest.fn().mockImplementation((param1) => {
-        switch (param1) {
-          case '0xd5e099c71b797516c10ed0f0d895f429c27811239832492184923841':
-            return Promise.reject(new Error('Whoops!'))
-          default:
-            return Promise.resolve(true)
+      getObject: function (param1) {
+        return {
+          createReadStream: jest.fn().mockImplementation((value) => {
+            switch (param1) {
+              case '0xd5e099c71b797516c10ed0f0d895f429c27811239832492184923841':
+                return Promise.reject(new Error('Whoops!'))
+              default:
+                return Promise.resolve(true)
+            }
+          })
         }
-      }),
-      listObjectsV2: jest.fn().mockImplementation((value) => {
-        switch (value) {
-          case '0xd5e099c71b797516c10ed0f0d895f429c2781146':
-            return false
-          default:
-            return true
-        }
-      }),
+      },
+      listObjectsV2: function (param1) {
+        return Promise.resolve(true)
+      },
       headBucket: jest.fn().mockImplementation((value) => {
         switch (value) {
           case '0xd5e099c71b797516c10ed0f0d895f429c2781146':
@@ -36,29 +32,5 @@ const AWS = {
         }
       })
     }
-  },
-  utils: {
-    isAddress: jest.fn().mockImplementation((value) => {
-      switch (value) {
-        case '0xd5e099c71b797516c10ed0f0d895f429c2781146':
-          return false
-        default:
-          return true
-      }
-    }),
-    getAddress: (param1) => { return param1 },
-    parseEther: () => { return 10000000000000000000 },
-    formatUnits: () => { return 10 },
-    Interface: function (abi) {
-      return {
-        encodeFunctionData: (param1, param2) => {
-          return '0x23b872dd0000000000000000000000008ba1f109551bd432803012645ac136ddd64dba72000000000000000000000000ab7c8803962c0f2f5bbbe3fa8bf41cd82aa1923c0000000000000000000000000000000000000000000000000de0b6b3a7640000'
-        }
-      }
-    }
   }
-}
-
-module.exports = {
-  AWS
 }
--- test/logic.test.js
+++ test/logic.test.js
@@ -2,51 +2,21 @@
 const path = require('path')
 const { getS3object, download, upload, bytesToNiceFormat, getS3directorylisting } = require(path.join(__dirname, '../logic/utils'))
 
-jest.mock("aws-sdk", () => {
-  return {
-    config: {
-      update() {
-        return {};
-      },
-    },
-    S3: function () {
-      return {
-        getObject: function(param1) {
-          return {
-            createReadStream: jest.fn().mockImplementation((value) => {
-              switch (param1) {
-                case '0xd5e099c71b797516c10ed0f0d895f429c27811239832492184923841':
-                  return Promise.reject(new Error('Whoops!'))
-                default:
-                  return Promise.resolve(true)
-              }
-            })
-          }
-        },
-        listObjectsV2: function(param1) {
-                return Promise.resolve(true)
-        },
-        headBucket: jest.fn().mockImplementation((value) => {
-            switch (value) {
-              case '0xd5e099c71b797516c10ed0f0d895f429c2781146':
-                return false
-              default:
-                return true
-            }
-          })
-      }
-    }
-  };
-});
+jest.mock('aws-sdk', () => {
+  const AWS = require('./__mock__/aws.js')
+  return AWS
+})
 jest.mock('../common/logger')
 jest.mock('winston')
 
 describe('getObject', function () {
-    test('should return true', async () => {
-      const success = await getS3object('test')
-      expect(success).toBe(true)
-    })
+  beforeEach(() => {
+    jest.resetModules()
+    process.env.AWS_BUCKET_NAME = 'test'
   })
-  
 
-  
+  test('should return true', async () => {
+    const success = await getS3object('test')
+    expect(success).toBe(true)
+  })
+})
